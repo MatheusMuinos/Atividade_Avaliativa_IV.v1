39f9ac238d38f6a7caa71c9db1b044bc94a6ffcb
Matheus Sarno Muiños <matheusmuinos@gmail.com>
2025-05-03 19:53:52 +0000

    feat: adicionando registro de usuario local ou em nuvem

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv'; // importar as variaveis de ambiente
 import db from './database/configdb.js'; // importar a conexao com o banco de dados
+import userRoutes from './routes/user.route.js'; // importar as rotas de usuario
 import User from './models/User.js'; // importar o modelo User
+
 
 dotenv.config(); // configurar as variaveis de ambiente
 db.connect(); // conectar ao banco de dados
 
 const app = express();
+
+
 app.use(express.json()); // middleware para interpretar JSON
 
+app.use("/users", userRoutes); // usar as rotas de usuario
 app.get('/', (req, res) => {   
     res.send({message:'Hello World!'});
 });
