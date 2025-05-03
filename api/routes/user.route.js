300f7d1b21cbc9cf9260bc55dad02bdf837e72a6
Matheus Sarno Muiños <matheusmuinos@gmail.com>
2025-05-03 20:28:48 +0000

    feat: adicionando o login e autenticacao

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,5 +4,6 @@ import userController from '../controller/user.controller.js';
 const router = express.Router();
 
 router.post('/register', userController.register);
-
+router.post('/login', userController.login);
+router.get('/users', userController.getAllUsers);
 export default router;
