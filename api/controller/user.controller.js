0440e8642953309505efe6e5a1984ea374737e86
Matheus Sarno Muiños <matheusmuinos@gmail.com>
2025-05-03 21:04:16 +0000

    adjust: retirando a exibição dos dados do novo usuario apos cadastro

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -3,7 +3,7 @@ import * as userService from '../services/user.service.js';
 const register = async (req, res) => {
     try {
         const user = await userService.registerUser(req.body);
-        res.status(200).json({ message: 'User registered successfully', user });
+        res.status(200).json({ message: 'User registered successfully'});
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
